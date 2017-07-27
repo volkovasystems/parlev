7d5cf3ff6d13d3c0917a2dbd90f0f613b0506c05
johnlenon <johnlenonmaghanoy@gmail.com>
2017-07-27 21:02:39 +0800

    Updated test.

--- parlev-test.js
+++ parlev-test.js
@@ -2,8 +2,10 @@
 const assert = require( "assert" );
 const parlev = require( "./parlev.js" );
 
-assert.deepEqual( parlev( [ [ 1, 2, 3 ] ] ), [ 1, 2, 3 ], "should be deeply equal" );
+assert.deepEqual( parlev( [ [ 1, 2, 3 ] ] ),
+	[ 1, 2, 3 ], "should be equal to [ 1, 2, 3 ]" );
 
-assert.deepEqual( parlev( [ [ 1, 2, 3 ], 1, 2, 3 ] ), [ [ 1, 2, 3 ], 1, 2, 3 ], "should be deeply equal" );
+assert.deepEqual( parlev( [ [ 1, 2, 3 ], 1, 2, 3 ] ), [ [ 1, 2, 3 ], 1, 2, 3 ],
+	"should be equal to [ [ 1, 2, 3 ], 1, 2, 3 ]" );
 
 console.log( "ok" );
